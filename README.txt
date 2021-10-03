9560ed4a0c1f1f843d1eda6c0bc45904abaa181d
Kevin Chen <kevin-chen@users.noreply.github.com>
2021-10-03 10:53:42 -0400

    clean up files

--- README.txt
+++ README.txt
@@ -1,1 +1,19 @@
 python3 sandbox.py
+
+Testing development answer corpus with predicted pos: 
+python3 WSJ_POS_CORPUS_FOR_STUDENTS/score.py WSJ_POS_CORPUS_FOR_STUDENTS/WSJ_24.pos RESULTS.pos
+
+Runs full training data and test on development set:
+python3 tagger.py WSJ_POS_CORPUS_FOR_STUDENTS/WSJ_02-21.pos WSJ_POS_CORPUS_FOR_STUDENTS/WSJ_24.words
+
+Runs smaller training data and test on smaller development set:
+python3 tagger.py WSJ_POS_CORPUS_FOR_STUDENTS/shorter.pos WSJ_POS_CORPUS_FOR_STUDENTS/shorter.words
+
+First Time: 
+21273 out of 32853 tags correct
+  accuracy: 64.752077
+
+Second Time: (1 / 1000 likelihood)
+22406 out of 32853 tags correct
+  accuracy: 68.200773
+
--- tagger.py
+++ tagger.py
@@ -1,4 +1,5 @@
 import numpy
+import sys
 
 def updateLikelihoodTable(likelihood, word, pos):
     posObject = likelihood.get(pos, None)
@@ -35,66 +36,49 @@ def updateTransitionProbabilities(transition):
             frequency[freq] = float(frequency[freq]) / float(count)
 
 def viterbi(tokens, likelihood, transition):
-    # N = number of POS
-    # T = number of token/words
-    N = len(likelihood)
-    T = len(tokens)
-    # create the 2d array, (N + 2) x T; POS by Words
+    N = len(likelihood) # number of POS
+    T = len(tokens) # number of token/words
     pos = list(likelihood.keys())
-    maxArr = [[0 for j in range(T)] for i in range(N)]
-    maxPosArr = [[0 for j in range(T)] for i in range(N)]
+    maxArr = [[0 for j in range(T)] for i in range(N)] # used for DP algorithm using previous word's probability
+    maxPosArr = [[0 for j in range(T)] for i in range(N)] # used to backtrack
 
-    # print(pos)
-
+    # runs viterbi on entire table 
     for word_index in range(T): # loop through each column
         word = tokens[word_index]
-        # print(word)
-        for pos_index in range(N): # loop through each part of speech
+        for pos_index in range(N): # loop through each part of speech/row
             p = pos[pos_index]
-            if word_index == 0:
-                t = 0
+            if word_index == 0: # first word in sentence has previous max of 1
+                t = 1 / 1000
                 if p in transition['Begin_Sent']:
                     t = transition['Begin_Sent'][p]
-                l = 0
+                l = 1 / 1000
                 if word in likelihood[p]:
                     l = likelihood[p][word]
                 a = t * l
-                # print(p, t, l, a)
                 maxArr[pos_index][word_index] = a
             else:
                 prev_column = word_index - 1
                 max_list = []
                 for prev_pos_index in range(N): # for each pos, loop through previous word's part of speech
                     prev_pos = pos[prev_pos_index]
-                    # print(word, prev_pos)
                     prev_probability = maxArr[prev_pos_index][prev_column]
-                    # if word == "an":
-                    #     print("Finding prev prob of", prev_pos, "to", p, "which is", prev_probability)
                     
                     if prev_probability != 0:
-                        # print(prev_pos)
-                        t = 0
+                        t = 1 / 1000
                         if p in transition[prev_pos]:
                             t = transition[prev_pos][p]
-                        l = 0
+                        l = 1 / 1000
                         if word in likelihood[p]:
                             l = likelihood[p][word]
                         a = prev_probability * t * l
-                        # if word == "an" and a != 0:
-                        #     print("a:", a, t, l)
-                        # if word == "an" and prev_pos == "IN" and p == "DT":
-                        #     print(a, prev_probability, t, l, prev_pos, p)
-                        # if word == "Haag":
-                        #     print(prev_pos_index, pos_index, p, t, l)
                         max_list.append(a)
                     else:
                         max_list.append(0)
-                max_index = max(range(len(max_list)), key=max_list.__getitem__)
-                # print(pos[max_index])
+                max_index = max(range(len(max_list)), key=max_list.__getitem__) # need max index to backtrack to previous column that gave max probability
                 maxArr[pos_index][word_index] = max(max_list)
                 maxPosArr[pos_index][word_index] = ((max_index, prev_column), pos[max_index])
-                # print(p,len(max_list))
-                # print(max(max_list))
+    
+    # handle the end state
     max_list = []
     prev_column = T - 1
     for prev_pos_index in range(N):
@@ -105,71 +89,54 @@ def viterbi(tokens, likelihood, transition):
             if 'End_Sent' in transition[prev_pos]:
                 t = transition[prev_pos]['End_Sent']
             a = prev_probability * t
-            # print(a, prev_probability, t)
             max_list.append(a)
         else:
             max_list.append(0)
     max_index = max(range(len(max_list)), key=max_list.__getitem__)
-    # print(max_list, max_index)
-    # print(pos[max_index])
-    # maxArr[pos_index][word_index] = max(max_list)
-    # maxPosArr[pos_index][word_index] = ((max_index, prev_column), pos[max_index])
-    # print(max_index, pos[max_index])
-    # print(len(max_list))
-    # arr[pos_index][word_index] = max(max_list)
-    
-    # print(maxArr)
 
-    # Backtrack
+    # Backtrack, traverse to find best path of best probabilities
     row, col = max_index, prev_column
     outputPOSSequence = []
-    # print(pos[max_index])
     outputPOSSequence.insert(0, pos[max_index])
     while col > 0:
         maxPos = maxPosArr[row][col]
         row = maxPos[0][0]
         col = maxPos[0][1]
-        # print(maxPos[1])
         outputPOSSequence.insert(0, maxPos[1])
 
-    # return maxPosArr
+    # output the sequence of POS that has best probability for this sentence
     return outputPOSSequence
 
+# given a word/line, update the likelihood of word being given POS and transition given previous POS
 def updatePriors(likelihood, transition, line, prevPos):
     line = line.strip()
-
     if line == "":
         updateTransitionTable(transition, prevPos, 'End_Sent')
         return "Begin_Sent"
     else:
-        # get the word and pos of line
-        parts = line.split("\t")
+        parts = line.split("\t") # get the word and pos of line
         word = parts[0]
         pos = parts[1]
-
-        # update the likelihood hashtable
-        updateLikelihoodTable(likelihood, word, pos)
-        # update the transition hashtable
-        updateTransitionTable(transition, prevPos, pos)
-        
+        updateLikelihoodTable(likelihood, word, pos) # update the likelihood hashtable
+        updateTransitionTable(transition, prevPos, pos) # update the transition hashtable
         return pos
 
+# reads the entire training file and updates each table
 def trainingData(trainingFile, transition, likelihood):
     prevPos = "Begin_Sent"
     transition['Begin_Sent'] = dict()
     transition['End_Sent'] = dict()
-
     line = trainingFile.readline()
-
     while line != None:
         if len(line) == 0:
             break
         prevPos = updatePriors(likelihood, transition, line, prevPos)
         line = trainingFile.readline()
-    
+    # convert counts to probabilities
     updateLikelihoodProbabilities(likelihood)
     updateTransitionProbabilities(transition)
 
+# read the development file and array output sentence of array words 
 def createSentences(developmentFile):
     line = developmentFile.readline()
     output = []
@@ -186,6 +153,7 @@ def createSentences(developmentFile):
         line = developmentFile.readline()
     return output
 
+# for each sentence, run viberti on that sentence and write file with each word and predicted pos tag
 def tagger(allSentences, transition, likelihood):
     outputFile = open("RESULTS.pos", "w")
     for sentence in allSentences:
@@ -201,29 +169,22 @@ def tagger(allSentences, transition, likelihood):
         outputFile.write("\n")
     outputFile.close()
 
-def main():
+def main(args):
+    trainingName = args[1]
+    developmentName = args[2]
+
     likelihood = dict()
     transition = dict()
 
-    trainingFile = open("WSJ_POS_CORPUS_FOR_STUDENTS/WSJ_02-21.pos", "r")
-    # trainingFile = open("WSJ_POS_CORPUS_FOR_STUDENTS/shorter.pos", "r")
+    trainingFile = open(trainingName, "r")
     trainingData(trainingFile, transition, likelihood)
     trainingFile.close()
 
-    # maxPosArr = viterbi(["Ms.", "Haag", "plays", "Elianti", "."], likelihood, transition)
-
-    # print("Likelihood", likelihood)
-    # print("")
-    # print("Transition", transition)
-
-    developmentFile = open("WSJ_POS_CORPUS_FOR_STUDENTS/WSJ_24.words", "r")
-    # developmentFile = open("WSJ_POS_CORPUS_FOR_STUDENTS/shorter.words", "r")
+    developmentFile = open(developmentName, "r")
     allSentences = createSentences(developmentFile)
     developmentFile.close()
 
     tagger(allSentences, transition, likelihood)
-    # print(viterbi(allSentences[0], likelihood, transition))
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__': sys.exit(main(sys.argv))
